700f3e56d15005d6dfd8da51f155daaa3e433daa
agus yogandi <36004309+agusyogandi@users.noreply.github.com>
2020-12-16 23:44:17 +0700

    nambahin superAdmin dan pengajuan iklan
    16/12/2020

---
+++ resources/views/admin/superAdmin.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TRADDEE.COM | Super Admin</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/userStyle.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <img src="assets/images/admin.png" class="imgAdmin"> <font color="white" size="4"> | Super Admin</font>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="nav pills ml-auto">
+    
+            <li class="nav-item">
+                <a class="nav-link" href="LandingPages"><font color="red">Logout</font></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+</br>
+</br>
+</br>
+
+</br>
+
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col-6">
+            <div class="card bg-dark">
+                <div class="card-body">
+                    <h3><font color="white">Aktivitas Admin</font></h3>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                    <center>
+                                        <h1>20.00<h1>
+                                        <h5>16-12-2020</h5>
+                                        <hr/>
+                                        <h5><strong>Login Terakhir</strong><h5>
+                                    </center>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                    <center>
+                                        <h1>20.45<h1>
+                                        <h5>16-12-2020</h5>
+                                        <hr/>
+                                        <h5><strong>Logout Terakhir</strong><h5>
+                                    </center>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5>Jumlah pengajuan iklan ke Admin</h5><hr/>
+                    <h3><font size="150">100</font></h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5>Jumlah iklan yang sudah di acc oleh admin</h5><hr/>
+                    <h3><font size="150">100</font></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+<hr/>
+<div class="card">
+    <div class="card-body">
+        <center><h5>Daftar pengajuan iklan ke admin</h5></center>
+        </br>
+        <table class="table table-bordered">
+            <thead  class="table-dark" >
+            <tr>
+                <th>Nama Toko</th>
+                <th>Tanggal Pengajuan</th>
+                <th>Tanggal Acc</th>
+                <th>Status</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Toko A</td>
+                <td>19-10-2020</td>
+                <td>22-10-2020</td>
+                <td><strong>Aktif</strong></td>
+            </tr>
+            <tr>
+            <td>Toko B</td>
+                <td>19-10-2020</td>
+                <td>22-10-2020</td>
+                <td><strong>Aktif</strong></td>
+            </tr>
+            <tr>
+            <td>Toko C</td>
+                <td>19-10-2020</td>
+                <td>22-10-2020</td>
+                <td><strong>Aktif</strong></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+</div>
+
+</br>
+
+
+
+  <center><p>Kisah Kreatif | Copyright 2020 © All Rights Reserved</p></center>
+</body>
+</html>
+
+
